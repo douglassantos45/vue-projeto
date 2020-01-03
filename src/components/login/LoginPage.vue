9d096c3cefa540fd06d7ffa3c5f881be4dcff585
Douglas Tott <douglasifbaianosb@gmail.com>
2020-01-03 11:50:25 -0300

    Alteraçao no Login

---
+++ src/components/login/LoginPage.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card class="elevation-12">
+              <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>Iniciar Sessão</v-toolbar-title>
+                <v-spacer />
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Usuário"
+                    name="login"
+                    prepend-icon="mdi-account"
+                    type="text"
+                  />
+
+                  <v-text-field
+                    id="password"
+                    label="Senha"
+                    name="password"
+                    prepend-icon="mdi-lock"
+                    type="password"
+                  />
+                </v-form>
+              </v-card-text>
+              <v-card-actions class="mr-2">
+                <v-btn color="purple" dark class="ml-2">Solicitar Login</v-btn>
+                <v-spacer />
+                <v-btn color="primary">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+<script>
+export default {
+  name: "LoginPage"
+};
+</script>
+
+<style>
+</style>
